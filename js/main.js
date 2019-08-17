ef19fd94aa62f9a36785365136c873f2d4d74063
April Walker <aprilcotmail@gmail.com>
2019-08-16 22:31:02 -0500

    redesign; home and blog update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
+  // oh no please don't look at this it's so horrible dear god why
   $('.drawer').drawer();
+  $("#h1").hide();
+  $("#h2").hide();
+
+  $("#im1").hide();
+
+  $("#p1").hide();
+  $("#p2").hide();
+  $("#p3").hide();
+  $("#p4").hide();
+  $("#p5").hide();
+
+  $("#h1").delay(0).fadeIn(1000);
+
+  $("#im1").delay(1000).fadeIn(1000);
+
+  $("#p1").delay(1000).fadeIn(1000);
+  $("#p2").delay(1000).fadeIn(1000);
+  $("#p3").delay(1000).fadeIn(1000);
+  $("#p4").delay(1000).fadeIn(1000);
+
+  $("#h2").delay(2000).fadeIn(1000);
+  $("#p5").delay(2000).fadeIn(1000);
+
+
 });
