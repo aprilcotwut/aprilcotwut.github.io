ede1ff6885f1cd44c95ca909c67625475dac7c00
April Walker <aprilcotmail@gmail.com>
2019-08-21 22:47:54 -0500

    profile pic

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   // oh no please don't look at this it's so horrible dear god why
   $('.drawer').drawer();
 
+  $("#i1").hide();
+
   $("#h1").hide();
   $("#h2").hide();
 
@@ -12,6 +14,8 @@ $(document).ready(function(){
   $("#p5").hide();
 
   $("#h1").delay(0).fadeIn(1000);
+
+  $("#i1").delay(1000).fadeIn(1000);
 
   $("#p1").delay(1000).fadeIn(1000);
   $("#p2").delay(1000).fadeIn(1000);
